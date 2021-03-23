58e3a8a9015c5e47215eeb5e64abad03b31c21b3
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-03-23 14:43:41 -0300

    Criação do método de listagem das tarefas "a fazer".

--- Task_Manager/src/control/MainController.java
+++ Task_Manager/src/control/MainController.java
@@ -2,7 +2,11 @@ package control;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,29 +14,89 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Task;
+import utils.TaskIterator;
 
 /**
  *
  * @author Cássio Figueiredo
  */
 public class MainController implements Initializable {
+    
+    private Task task;
+    private TaskIterator it;
 
     @FXML
-    private TableView<?> tblToDo;
+    private TableView<Task> tblToDo;
     @FXML
-    private TableView<?> tblInProgress;
+    private TableView<Task> tblInProgress;
     @FXML
-    private TableView<?> tblDone;
+    private TableView<Task> tblDone;
     @FXML
     private Button btnNewTask;
+    @FXML
+    private TableColumn<Task, String> idToDo;
+    @FXML
+    private TableColumn<Task, String> nameToDo;
+    @FXML
+    private TableColumn<Task, String> dateToDo;
+    @FXML
+    private TableColumn<Task, String> idProgress;
+    @FXML
+    private TableColumn<Task, String> nameProgress;
+    @FXML
+    private TableColumn<Task, String> dateProgress;
+    @FXML
+    private TableColumn<Task, String> idDone;
+    @FXML
+    private TableColumn<Task, String> nameDone;
+    @FXML
+    private TableColumn<Task, String> dateDone;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        initializeToDoTable();
+        
+    }
+    
+    private void initializeToDoTable() {
+        idToDo.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        nameToDo.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        dateToDo.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        
+        tblToDo.setItems(getTasksToDo());
+    }
+    
+    private ObservableList<Task> getTasksToDo() {
+        ObservableList<Task> tasks = FXCollections.observableArrayList();
+        
+        task = new Task();
+        it = new TaskIterator(task.getTasks());
+        
+        while (it.hasNext()) {
+            Task tsk = (Task) it.next();
+            
+            if (isTaskToDo(tsk.getSituation())) {
+                tasks.add(tsk);
+            }
+        }
+        
+        return tasks;
+    }
+    
+    private boolean isTaskToDo(String situation) {
+        if (situation.endsWith("To Do")) {
+            return true;
+            
+        } else {
+            return false;
+        }
+    }
 
     @FXML
     private void btnNewTaskAction(ActionEvent event) throws IOException {
