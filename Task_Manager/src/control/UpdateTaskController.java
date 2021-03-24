5f3b1b360b5c48ad2975f687cb6c9ed96d841758
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-03-23 22:16:47 -0300

    Criação da tela de update.

--- Task_Manager/src/control/UpdateTaskController.java
+++ Task_Manager/src/control/UpdateTaskController.java
@@ -1,7 +1,10 @@
 package control;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import model.Task;
 
 /**
  * FXML Controller class
@@ -16,7 +20,9 @@ import javafx.scene.control.TextField;
  * @author Cássio Figueiredo
  */
 public class UpdateTaskController implements Initializable {
-
+    
+    private Task selectedTask;
+    
     @FXML
     private TextField txtName;
     @FXML
@@ -25,15 +31,41 @@ public class UpdateTaskController implements Initializable {
     private ComboBox<String> cbxSituation;
     @FXML
     private Button btnUpdate;
+    @FXML
+    private TextField txtId;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        cbxSituation.setItems(getOptions());
     }    
-
+    
+    private ObservableList<String> getOptions() {
+        ObservableList<String> options = FXCollections.observableArrayList();
+        
+        options.add("To Do");
+        options.add("In Progress");
+        options.add("Done");
+        
+        return options;
+    }
+    
+    public void initData(Task task) {
+        selectedTask = task;
+        setFields(selectedTask);
+    }
+    
+    private void setFields(Task task) {
+        LocalDate localDate = null;
+        
+        txtId.setText(String.valueOf(task.getId()));
+        txtName.setText(task.getName());
+        txtDate.setValue(LocalDate.parse(task.getDate()));
+        cbxSituation.setValue(task.getSituation());
+    }
+    
     @FXML
     private void btnUpdateAction(ActionEvent event) {
     }
