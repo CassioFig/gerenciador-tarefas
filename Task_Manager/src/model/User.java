d6d795c7be8327a2894d87397594beead87c7b7e
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-04-16 01:27:34 -0300

    Criação do método de encriptação de senhas.

--- Task_Manager/src/model/User.java
+++ Task_Manager/src/model/User.java
@@ -1,12 +1,19 @@
 package model;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Cássio Figueiredo
  */
 public class User {
     private int id;
-    private final String user, password;
+    private final String user;
+    private String password;
 
     public User(String user, String password) {
         this.user = user;
@@ -14,7 +21,32 @@ public class User {
     }
     
     public void saveUser() {
+        this.password = this.encryptPassword(this.password);
+    }
+    
+    private String encryptPassword(String password) {
         
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = algorithm.digest(password.getBytes("UTF-8"));
+            
+            return toHex(messageDigest);
+           
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+    private String toHex(byte[] password) {
+        StringBuilder hexString = new StringBuilder();
+        
+        for (byte b : password) {
+            hexString.append(String.format("%02X", 0xFF & b));
+        }
+        
+        return hexString.toString();
     }
 
     public int getId() {
